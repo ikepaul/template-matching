1ec0c64ecbc8c79570c5c379935438dfb406d12d
IKE <paul.e.soukup@gmail.com>
2022-10-02 18:05:15 +0200

    tanimoto function done

--- shape.hs
+++ shape.hs
@@ -1,5 +1,20 @@
 module Shape where
 
-import Graphics.UI.Threepenny
+import Graphics.UI.Threepenny hiding(map, div)
+import Draw
 
 type Shape = [Point]
+
+hausdorrf :: Point -> Shape -> Double
+hausdorrf (px, py) = minimum . map (\(spx, spy) -> sqrt ((px - spx)^2  + (py - spy)^2))
+
+tanimoto :: Shape -> Shape -> Double
+tanimoto s1 s2 = snd result / fst result where
+  result = count $ n ++ m
+  count = foldl (\b a -> (fst b + 1, snd b + (if a then 1 else 0))) (0,0)
+  n = overlapping s1 s2
+  m = overlapping s2 s1
+  overlapping a b = map (\sp -> 0.1*canvasWidth > hausdorrf sp b) a 
+
+
+threshHold = 0.1
